a40f11e432d9879194a49ba6d19957e776633c50
lanitaleon <1912164282@qq.com>
2015-03-19 09:40:29 +0800

    接口暂定这么多-佳爷看一眼还需要什么-或者需要什么注释解释

--- NJUSENBA/src/main/java/logic/PlayerService.java
+++ NJUSENBA/src/main/java/logic/PlayerService.java
@@ -1,4 +1,21 @@
 package logic;
 
+import java.util.ArrayList;
+
+import vo.PlayerPersonalInfoVO;
+import vo.PlayerVO;
+
 public interface PlayerService {
+	//TODO 所有队员的所有原始总赛季数据
+	public ArrayList<PlayerVO> allPlayerSeasonData();
+	//TODO 所有队员的所有原始场均数据
+	public ArrayList<PlayerVO> allPlayerAverageData();
+	// 所有球员按照某一项元素的升序排列
+	public ArrayList<PlayerVO> sort_Ascend(int key, ArrayList<PlayerVO> playerList);
+	// 所有球员按照某一项元素的降序排列
+	public ArrayList<PlayerVO> sort_Descend(int key, ArrayList<PlayerVO> playerList);
+	//TODO 筛选部分
+	public ArrayList<PlayerVO> filter(String playerPosition, String league, String SortBy);
+	//TODO 球员个人信息
+	public ArrayList<PlayerPersonalInfoVO> playerPersonalInfo();
 }
--- NJUSENBA/src/main/java/logic/TeamService.java
+++ NJUSENBA/src/main/java/logic/TeamService.java
@@ -1,5 +1,19 @@
 package logic;
 
-public interface TeamService {
+import java.util.ArrayList;
 
+import vo.TeamInfoVO;
+import vo.TeamVO;
+
+public interface TeamService {
+	//TODO 所有球队的原始总赛季数据
+	public ArrayList<TeamVO> allTeamSeasonData();
+	//TODO 所有球队的原始场均数据
+	public ArrayList<TeamVO> allTeamAverageData();
+	//所有队伍信息-升序排列
+	public ArrayList<TeamVO> sort_Ascend(int key, ArrayList<TeamVO> teamList);
+	//所有队伍信息-降序排列
+	public ArrayList<TeamVO> sort_Descend(int key, ArrayList<TeamVO> teamList);
+	//TODO 球队具体信息比如球队全名等
+	public ArrayList<TeamInfoVO> teamDetailInfo();
 }

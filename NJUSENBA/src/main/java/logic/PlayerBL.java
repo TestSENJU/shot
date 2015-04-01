07132516f38b112d4b4342cadca9c1b291d091dd
lanitaleon <1912164282@qq.com>
2015-04-01 17:36:34 +0800

    感人QAQ我好像写完了

--- NJUSENBA/src/main/java/logic/PlayerBL.java
+++ NJUSENBA/src/main/java/logic/PlayerBL.java
@@ -44,14 +44,36 @@ public class PlayerBL implements PlayerService {
 					assistingNum, blockShotNum, foulNum, muffNum);
 			double trueHitRate = cplay.trueHitRate(score, scoresAll[1], scoresAll[5]);
 			double shootEfficiency = cplay.shootEfficiency(scoresAll[0], scoresAll[2], scoresAll[1]);
-			double reboundRate = 0;//TODO
-			double offensiveReboundRate = 0;
-			double defensiveReboundRate = 0;
-			double assistingRate = 0;
-			double stealRate = 0;
-			double blockShotRate = 0;
-			double muffRate = 0;
-			double usage = 0;			
+			//TODO teamAll competeAll
+			double[] teamAll = playAllList.get(key).getTeamAll();
+			double[] competeAll = playAllList.get(key).getCompeteAll();
+			//篮板率=球员篮板数*(球队所有球员上场时间/5)/球员上场时间/(球队总篮板+对手总篮板)
+			String[] timeTeam = playAllList.get(key).getTimeTeam().split(":");
+			double minT = Double.valueOf(timeTeam[0]);
+			double secondT = Double.valueOf(timeTeam[1]);
+			String[] ptime = playAllList.get(key).getTimeAll().split(":");
+			double min = Double.valueOf(ptime[0]);
+			double second = Double.valueOf(ptime[1]);
+			double reboundRate = scoresAll[8]*((minT*60+secondT)/5)/(min*60+second)/(teamAll[6]+competeAll[6]);
+			double offensiveReboundRate = scoresAll[6]*((minT*60+secondT)/5)/(min*60+second)/(teamAll[4]+competeAll[4]);
+			double defensiveReboundRate = scoresAll[7]*((minT*60+secondT)/5)/(min*60+second)/(teamAll[5]+competeAll[5]);
+			//助攻率=球员助攻数/(球员上场时间/(球队所有球员上场时间/5)*球队总进球数-球员进球数)
+			double assistingRate = scoresAll[9]/((min*60+second)/((minT*60+secondT)/5)*teamAll[1]-scoresAll[0]);
+			//抢断率 = 球员抢断数*(球队所有球员上场时间/5)/球员上场时间/对手进攻次数
+			//对手进攻次数=对手进攻回合
+			//对手进攻回合=对手投篮数+0.4*对手球队罚球数
+			//            -1.07*(对手进攻篮板/(对手进攻篮板+我方防守篮板)*对手投失球数)
+			//             +1.07*对手失误数
+			double competeRound = competeAll[0]+0.4*competeAll[3]-1.07*(competeAll[4]/(competeAll[4]+teamAll[5])*(competeAll[0]-competeAll[1]))+competeAll[7];
+			double stealRate = scoresAll[10]*((minT*60+second)/5)/(min*60+second)/competeRound;
+			//盖帽率=球员盖帽数*(球队所有球员上场时间/5)/球员上场时间/对手两分球出手次数
+			double blockShotRate = scoresAll[11]*((minT*60+second)/5)/(min*60+second)/(scoresAll[1]-scoresAll[3]);
+			//失误率 = 球员失误数/(球员两分球出手次数+0.44*球员罚球次数+球员失误数)
+			double muffRate = scoresAll[12]/(scoresAll[1]-scoresAll[3]+0.44*scoresAll[5]+scoresAll[12]);
+			//使用率 = (球员出手次数+0.44*球员罚球次数+球员失误次数)
+			//        *(球队所有球员上场时间/5)/球员上场时间
+			//         /(球队所有球员总出手次数+0.44*球队所有球员罚球次数+球队所有球员失误次数)
+			double usage = (scoresAll[1]+0.44*scoresAll[5]+scoresAll[12])*((minT*60+second)/5)/(min*60+second)/(teamAll[0]+0.44*teamAll[3]+teamAll[7]);			
 			playerList.add(new PlayerVO(playAllList.get(key).getPlayerName(),
 					playAllList.get(key).getTeam(),
 					playAllList.get(key).getNumOfMatches(),
@@ -65,13 +87,48 @@ public class PlayerBL implements PlayerService {
 					blockShotRate, muffRate, usage
 					));
 		}
-
 		return playerList;
 	}
 	//TODO 所有队员的所有原始场均数据
 	public ArrayList<PlayerAverageVO> allPlayerAverageData(){
+		//all season data for average calculation
+		ArrayList<PlayerVO> pAllList = allPlayerSeasonData();
 		ArrayList<PlayerAverageVO> playerList = new ArrayList<PlayerAverageVO>();
-	
+		for (int i=0;i < pAllList.size();i++) {
+			String[] time = pAllList.get(i).getPlayingTime().split(":");
+			double min = Double.valueOf(time[0]);
+			double second = Double.valueOf(time[1]);
+			double competeN = pAllList.get(i).getCompeteNum();
+			playerList.add(new PlayerAverageVO(pAllList.get(i).getName(),
+					pAllList.get(i).getTeams(),
+					pAllList.get(i).getCompeteNum()/competeN,
+				    (pAllList.get(i).getOffensiveNum()+0.0)/competeN,
+					pAllList.get(i).getReboundNum()/competeN,
+					pAllList.get(i).getAssistingNum()/competeN,
+					Double.toString((min*60+second)/competeN),
+					pAllList.get(i).getHitRate()/competeN,
+					pAllList.get(i).getThreePointHitRate()/competeN,
+					pAllList.get(i).getFreeThrowHitRate()/competeN,
+					pAllList.get(i).getOffensiveReboundNum()/competeN,
+					pAllList.get(i).getDefensiveReboundNum()/competeN,
+					pAllList.get(i).getStealNum()/competeN,
+					pAllList.get(i).getBlockShotNum()/competeN,
+					pAllList.get(i).getMuffNum()/competeN,
+					pAllList.get(i).getFoulNum()/competeN,
+					pAllList.get(i).getScore()/competeN,
+					pAllList.get(i).getEfficiency()/competeN,
+					pAllList.get(i).getGmScEfficiency()/competeN,
+					pAllList.get(i).getTrueHitRate()/competeN,
+					pAllList.get(i).getShootEfficiency()/competeN,
+					pAllList.get(i).getReboundRate()/competeN,
+					pAllList.get(i).getOffensiveReboundRate()/competeN,
+					pAllList.get(i).getDefensiveReboundRate()/competeN,
+					pAllList.get(i).getAssistingRate()/competeN,
+					pAllList.get(i).getStealRate()/competeN,
+					pAllList.get(i).getBlockShotRate()/competeN,
+					pAllList.get(i).getMuffRate()/competeN,
+					pAllList.get(i).getUsage()/competeN));
+		}
 		return playerList;
 	}
 	// 所有球员按照某一项元素的升序排列
@@ -108,14 +165,15 @@ public class PlayerBL implements PlayerService {
 		playerList = fp.filterSortBy(info[0],info[1],info[2]);
 		return playerToString(playerList);		
 	}
+
+	// 球员个人信息
 	public ArrayList<PlayerStrVO> filterAverage(String filterInfo){
 		ArrayList<PlayerAverageVO> playerList = new ArrayList<PlayerAverageVO>();
 		String[] info = filterInfo.split(",");
-		FliterPlayerAverage fpa = new FliterPlayerAverage();
-		playerList = fpa.filterSortBy(info[0], info[1], info[2]);
-		return playerAverageToString(playerList);
+		FliterPlayerAverage fp = new FliterPlayerAverage();
+		playerList = fp.filterSortBy(info[0],info[1],info[2]);
+		return playerAverageToString(playerList);	
 	}
-	// 球员个人信息
 	public ArrayList<PlayerPersonalInfoVO> playerPersonalInfo(){
 		ArrayList<PlayerPersonalInfoVO> playerInfoList = new ArrayList<PlayerPersonalInfoVO>();
 		PlayerData pd = new PlayerData_Impl();

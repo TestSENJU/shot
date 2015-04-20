6fd08812d698ff482548acb600ed57f0ec0894f2
Jason <Jason@172.26.69.190>
2015-04-20 17:54:48 +0800

    同步版本

--- IterationSecond/src/main/java/logic/allPlayerInformation.java
+++ IterationSecond/src/main/java/logic/allPlayerInformation.java
@@ -1,5 +1,13 @@
 package logic;
 
+import java.util.ArrayList;
+
+import VO.playerInformationVO;
+
 public class allPlayerInformation {
-     
+     	public String[] getAllPlayerInformation(){
+     		ArrayList<playerInformationVO> playerlist=new ArrayList<playerInformationVO>();
+     		String[] playerInformation=null;
+     		return playerInformation;
+     	}
 }
